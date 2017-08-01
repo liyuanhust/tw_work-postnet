3544f078bb43c99083bac64649267217eabd2d11
Yuan Li <ywli@CNywli.local>
2017-08-02 00:27:02 +0800

    单个数字转条码-- 完善测试用例，代码开发，测试用例通过

--- java/maven/src/main/java/com/tw/post/Postnet.java
+++ java/maven/src/main/java/com/tw/post/Postnet.java
@@ -2,6 +2,39 @@ package com.tw.post;
 
 public class Postnet {
     public String convertDigit(int digit) {
-        return "";
+        String barcode = "";
+        switch (digit) {
+            case 1:
+                barcode = ":::||";
+                break;
+            case 2:
+                barcode = "::|:|";
+                break;
+            case 3:
+                barcode = ":::||";
+                break;
+            case 4:
+                barcode = ":|::|";
+                break;
+            case 5:
+                barcode = ":|:|:";
+                break;
+            case 6:
+                barcode = ":||::";
+                break;
+            case 7:
+                barcode = "|:::|";
+                break;
+            case 8:
+                barcode = "|::|:";
+                break;
+            case 9:
+                barcode = "|:|::";
+                break;
+            case 0:
+                barcode = "||:::";
+                break;
+        }
+        return barcode;
     }
 }
--- java/maven/src/test/java/PostnetTest.java
+++ java/maven/src/test/java/PostnetTest.java
@@ -5,42 +5,113 @@ import static org.fest.assertions.api.Assertions.assertThat;
 
 public class PostnetTest {
     @Test
-    public void should_return_barcode_given_digit(int digit) {
-        String barcode = "";
-        switch (digit) {
-            case 1:
-                barcode = ":::||";
-                break;
-            case 2:
-                barcode = "::|:|";
-                break;
-            case 3:
-                barcode = ":::||";
-                break;
-            case 4:
-                barcode = ":|::|";
-                break;
-            case 5:
-                barcode = ":|:|:";
-                break;
-            case 6:
-                barcode = ":||::";
-                break;
-            case 7:
-                barcode = "|:::|";
-                break;
-            case 8:
-                barcode = "|::|:";
-                break;
-            case 9:
-                barcode = "|:|::";
-                break;
-            case 0:
-                barcode = "||:::";
-                break;
-        }
+    public void should_return_barcode_given_1(){
+        //given
+        int digit = 1;
+        //when
         Postnet postnet = new Postnet();
-        assertThat(postnet.convertDigit(digit)).isEqualTo(barcode);
+        String barcode = postnet.convertDigit(digit);
+        //then
+        assertThat(barcode).isEqualTo(":::||");
+    }
+
+    @Test
+    public void should_return_barcode_given_2(){
+        //given
+        int digit = 2;
+        //when
+        Postnet postnet = new Postnet();
+        String barcode = postnet.convertDigit(digit);
+        //then
+        assertThat(barcode).isEqualTo("::|:|");
+    }
+
+    @Test
+    public void should_return_barcode_given_3(){
+        //given
+        int digit = 3;
+        //when
+        Postnet postnet = new Postnet();
+        String barcode = postnet.convertDigit(digit);
+        //then
+        assertThat(barcode).isEqualTo(":::||");
+    }
+
+    @Test
+    public void should_return_barcode_given_4(){
+        //given
+        int digit = 4;
+        //when
+        Postnet postnet = new Postnet();
+        String barcode = postnet.convertDigit(digit);
+        //then
+        assertThat(barcode).isEqualTo(":|::|");
+    }
+
+    @Test
+    public void should_return_barcode_given_5(){
+        //given
+        int digit = 5;
+        //when
+        Postnet postnet = new Postnet();
+        String barcode = postnet.convertDigit(digit);
+        //then
+        assertThat(barcode).isEqualTo(":|:|:");
+    }
+
+    @Test
+    public void should_return_barcode_given_6(){
+        //given
+        int digit = 6;
+        //when
+        Postnet postnet = new Postnet();
+        String barcode = postnet.convertDigit(digit);
+        //then
+        assertThat(barcode).isEqualTo(":||::");
+    }
+
+    @Test
+    public void should_return_barcode_given_7(){
+        //given
+        int digit = 7;
+        //when
+        Postnet postnet = new Postnet();
+        String barcode = postnet.convertDigit(digit);
+        //then
+        assertThat(barcode).isEqualTo("|:::|");
+    }
+
+    @Test
+    public void should_return_barcode_given_8(){
+        //given
+        int digit = 8;
+        //when
+        Postnet postnet = new Postnet();
+        String barcode = postnet.convertDigit(digit);
+        //then
+        assertThat(barcode).isEqualTo("|::|:");
+    }
+
+    @Test
+    public void should_return_barcode_given_9(){
+        //given
+        int digit = 9;
+        //when
+        Postnet postnet = new Postnet();
+        String barcode = postnet.convertDigit(digit);
+        //then
+        assertThat(barcode).isEqualTo("|:|::");
+    }
+
+    @Test
+    public void should_return_barcode_given_0(){
+        //given
+        int digit = 0;
+        //when
+        Postnet postnet = new Postnet();
+        String barcode = postnet.convertDigit(digit);
+        //then
+        assertThat(barcode).isEqualTo("||:::");
     }
 
 }
