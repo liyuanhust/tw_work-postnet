910ff30623bfac7eca3e6f4503e71ebb29e0a253
Yuan Li <ywli@CNywli.local>
2017-08-02 00:44:41 +0800

    新增功能，检查输入邮编的有效性

--- java/maven/src/main/java/com/tw/post/Postnet.java
+++ java/maven/src/main/java/com/tw/post/Postnet.java
@@ -5,21 +5,45 @@ import java.util.Map;
 
 public class Postnet {
     private static final Map<Integer, String> digitMap = new HashMap<Integer, String>();
-    static{
-        digitMap.put(1,":::||");
-        digitMap.put(2,"::|:|");
-        digitMap.put(3,"::||:");
-        digitMap.put(4,":|::|");
-        digitMap.put(5,":|:|:");
-        digitMap.put(6,":||::");
-        digitMap.put(7,"|:::|");
-        digitMap.put(8,"|::|:");
-        digitMap.put(9,"|:|::");
-        digitMap.put(0,"||:::");
+
+    static {
+        digitMap.put(1, ":::||");
+        digitMap.put(2, "::|:|");
+        digitMap.put(3, "::||:");
+        digitMap.put(4, ":|::|");
+        digitMap.put(5, ":|:|:");
+        digitMap.put(6, ":||::");
+        digitMap.put(7, "|:::|");
+        digitMap.put(8, "|::|:");
+        digitMap.put(9, "|:|::");
+        digitMap.put(0, "||:::");
     }
 
 
     public String convertDigit(int digit) {
         return digitMap.get(digit);
+    }
+
+    public boolean check(String post) {
+        if (post == null || post.length() == 0) {
+            return false;
+        }
+        int numberOfDigits = post.length();
+        if (numberOfDigits != 5
+                && numberOfDigits != 9
+                && numberOfDigits != 10) {
+            return false;
+        }
+        for (int i = 0; i < numberOfDigits; i++) {
+            char digit = post.charAt(i);
+            if (digit >= '0' && digit <= '9') {
+                continue;
+            }
+            if (digit == '-') {
+                continue;
+            }
+            return false;
+        }
+        return true;
     }
 }
